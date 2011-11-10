0bde650cc33d20bdfd6ee2601d4e86fb83527821
Luca Matteis <lmatteis@gmail.com>
2011-11-10 12:25:48 +0100

    Fixed and improved scraping for CIAT's website

--- ciat.js
+++ ciat.js
@@ -2,7 +2,7 @@ var scraper = require('scraper');
 var sys = require('util'),
     couchdb = require('felix-couchdb'),
     client = couchdb.createClient(80, 'seeds.iriscou.ch', process.argv[2], process.argv[3]),
-    db = client.db("accessions");
+    db = client.db("ciat");
 
 function clean(str) {
     return str.replace(/\s+/g, ' ');
@@ -12,35 +12,43 @@ var offset = process.argv[4] || 0;
 var nextLink = false;
 
 var mcpd = {
-    "Accession number": "ACCENUMB",
-    "Common names": "ACCENAME",
-    "Collection code": "COLLNUMB",
-    "Genus": "GENUS",
-    "Species": "SPECIES",
-    "Date of collection ( dd-mm-yyyy )": "ACQDATE",
-    "Country": "ORIGCTY",
-    "Latitude (decimal)": "LATITUDE",
-    "Longitude (decimal)": "LONGITUDE"
+    "(Identification) Accession number": "ACCENUMB",
+    "(Identification) Common names": "ACCENAME",
+    "(Collection information) Collection code": "COLLNUMB",
+    "(Taxonomic information) Genus": "GENUS",
+    "(Taxonomic information) Species": "SPECIES",
+    "(Collection information) Date of collection ( dd-mm-yyyy )": "ACQDATE",
+    "(Collection information) Country": "ORIGCTY",
+    "(Collection information) Latitude (decimal)": "LATITUDE",
+    "(Collection information) Longitude (decimal)": "LONGITUDE"
 };
+
+var cropname = "Forages";
 
 scraper(
     {
-       'uri': 'http://isa.ciat.cgiar.org/urg/showbsearchresults.do?pager.offset='+offset
+       'uri': 'http://isa.ciat.cgiar.org/urg/showfsearchresults.do?pager.offset='+offset
            , 'headers': {
                 'User-Agent':  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:7.0.1) Gecko/20100101 Firefox/7.0.1',
                 'Accept-Language': 'en-us,en;q=0.5',
                 'Accept-Encoding': 'gzip, deflate',
                 'Accept-Charset':  'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
                 'Referer': 'http://isa.ciat.cgiar.org/urg/showbsearchresults.do?pager.offset=0',
-                'Cookie': 'JSESSIONID=C57BC8F12DD3FF48369B9C28FE7D4371; __utma=148326703.1618406730.1320052661.1320228702.1320239085.8; __utmc=148326703; __utmz=148326703.1320052661.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmb=148326703.7.10.1320239085'
-           }
+                'Cookie': 'JSESSIONID=DCAD1180DB0E84912A4DB755585EFC06'
+           },
+           'encoding': "binary"
     }
     , function(err, $) {
         if (err) {throw err}
 
-        var $children = $("table.marcos").children(),
-            $topcols = $children.eq(1).find("th"),
-            $cols = $children.eq(2).find("th");
+        var $children = $("table.marcos").children();
+        if(cropname == "Forages") {
+            var $topcols = $children.eq(0).find("th"),
+                $cols = $children.eq(1).find("th");
+        } else {
+            var $topcols = $children.eq(1).find("th"),
+                $cols = $children.eq(2).find("th");
+        }
 
         var cols = [],
             offset = 0;
@@ -54,11 +62,14 @@ scraper(
                 cols.push(col);
             } else if(colspan) { // go and check the other tr
                 colspan = parseInt(colspan, 10);
+                var topname = "(" + col + ")";
                 for(var i=offset; i < (offset + colspan); i++) {
-                    col = clean($cols.eq(i).text().trim());
+                    col = topname + " " + clean($cols.eq(i).text().trim());
                     cols.push(col);
                 }
                 offset += colspan;
+            } else {
+                cols.push(col);
             }
 
         });
@@ -69,9 +80,9 @@ scraper(
             var $cell = $(this).find("td");
             $cell.each(function(idx){
                 var c = cols[idx];
-                if(c == "Trip report") {
+                if(c == "(Collection information) Trip report") {
                     var val = $(this).find("img").attr("onclick");
-                } else if(c == "Seed/Plant") {
+                } else if(c == "Seed/Plant" || c == "(Allele position for the Locus EST-1) Ref. Gel" || c == "(Allele position for the Locus EST-1) Gel" || c == "(Photo) Seed/Plant" || c == "(Photo) Herbarium" || c == "(Photo) Flower") {
                     var val = $(this).find("a").attr("href");
                 } else {
                     var val = clean($(this).text().trim());
@@ -81,11 +92,12 @@ scraper(
                 if(val)
                     acc[c] = val;
             });
-            acc["CROPNAME"] = "Bean";
+            acc["CROPNAME"] = cropname;
             acc["INSTCODE"] = "CIAT";
-            //console.log(acc);
-            db.saveDoc(acc["INSTCODE"] + "_" + acc["ACCENUMB"], acc, function(er, ok) {
-                //if (er) throw new Error(JSON.stringify(er));
+            var docid = encodeURIComponent(acc["INSTCODE"] + "_" + acc["ACCENUMB"]);
+            //console.log(docid);
+            db.saveDoc(docid, acc, function(er, ok) {
+                //if (er) throw er;
             });
         });
 
